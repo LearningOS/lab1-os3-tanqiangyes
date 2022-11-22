49e14a816a0ddf7ab891fc2e1526144bd1d6a374
tanqiangyes <826285820@qq.com>
2022-11-22 06:01:03 +0000

    test3 over

--- os3/src/timer.rs
+++ os3/src/timer.rs
@@ -4,6 +4,7 @@ use riscv::register::time;
 
 const TICKS_PER_SEC: usize = 100;
 const MICRO_PER_SEC: usize = 1_000_000;
+const MILLIS_PER_SEC: usize = 1_000;
 
 pub fn get_time() -> usize {
     time::read()
@@ -15,4 +16,8 @@ pub fn get_time_us() -> usize {
 
 pub fn set_next_trigger() {
     set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
+}
+
+pub fn get_time_ms() -> usize {
+    get_time_us() / MILLIS_PER_SEC
 }
